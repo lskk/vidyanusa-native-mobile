2e5b53f102af0f71bd6741a9a59d5050742da6fd
Hendra Permana <hynra@yahoo.com>
2017-04-22 22:47:33 +0700

    react-native-router-flux test [ok]

--- app/index.js
+++ app/index.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+
+import ScarletScreen from './screens/ScarletScreen';
+import GrayScreen from './screens/GreyScreen';
 
 const App = () => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.welcome}>
-                Welcome to the Demo!
-            </Text>
-        </View>
+        <Router>
+            <Scene key="root">
+                <Scene key="scarlet"
+                       component={ScarletScreen}
+                       title="Scarlet"
+                       initial
+                />
+                <Scene
+                    key="gray"
+                    component={GrayScreen}
+                    title="Gray"
+                />
+            </Scene>
+        </Router>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-});
 
 export default App;
