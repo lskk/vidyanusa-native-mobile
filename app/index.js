fda31de7b1e975db28020bc1ac1d2da3893c4615
Hendra Permana <hynra@yahoo.com>
2017-04-22 23:05:12 +0700

    router-flux tabs test [ok]

--- app/index.js
+++ app/index.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
 import ScarletScreen from './screens/ScarletScreen';
 import GrayScreen from './screens/GreyScreen';
+import BlueScreen from './screens/BlueScreen';
+import MaizeScreen from './screens/MaizeScreen';
+import GoldScreen from './screens/GoldScreen';
+import BlackScreen from './screens/BlackScreen';
+
+const TabIcon = ({ selected, title }) => {
+    return (
+        <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+    );
+}
 
 const App = () => {
     return (
         <Router>
             <Scene key="root">
-                <Scene key="scarlet"
-                       component={ScarletScreen}
-                       title="Scarlet"
-                       initial
-                />
+                {/* Tab Container */}
                 <Scene
-                    key="gray"
-                    component={GrayScreen}
-                    title="Gray"
-                />
+                    key="tabbar"
+                    tabs={true}
+                    tabBarStyle={{ backgroundColor: '#FFFFFF' }}
+                >
+                    {/* Tab and it's scenes */}
+                    <Scene key="osu" title="OSU" icon={TabIcon}>
+                        <Scene key="scarlet"
+                               component={ScarletScreen}
+                               title="Scarlet"
+                        />
+                        <Scene
+                            key="gray"
+                            component={GrayScreen}
+                            title="Gray"
+                        />
+                    </Scene>
+
+                    {/* Tab and it's scenes */}
+                    <Scene key="um" title="UM" icon={TabIcon}>
+                        <Scene
+                            key="blue"
+                            component={BlueScreen}
+                            title="Blue"
+                        />
+                        <Scene
+                            key="maize"
+                            component={MaizeScreen}
+                            title="Maize"
+                        />
+                    </Scene>
+
+                    {/* Tab and it's scenes */}
+                    <Scene key="vu" title="VU" icon={TabIcon}>
+                        <Scene
+                            key="gold"
+                            component={GoldScreen}
+                            title="Gold"
+                        />
+                        <Scene
+                            key="black"
+                            component={BlackScreen}
+                            title="Black"
+                        />
+                    </Scene>
+                </Scene>
             </Scene>
         </Router>
     );
